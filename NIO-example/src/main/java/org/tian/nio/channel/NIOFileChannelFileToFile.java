ee4788131eb8ba1d6daabcb2ca223100fbd33015
MengHao <907749695@qq.com>
2021-10-26 15:00:35 +0800

    feat: NIO通道读文件案例2,通过buffer进行转换

---
+++ NIO-example/src/main/java/org/tian/nio/channel/NIOFileChannelFileToFile.java
@@ -0,0 +1,38 @@
+package org.tian.nio.channel;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author tianmh
+ * @date create by 2021/10/26 14:41
+ */
+public class NIOFileChannelFileToFile {
+    private static final String ROOT_PATH = System.getProperty("user.dir");
+//1.txt先自己写入内容并创建
+    public static void main(String[] args) throws Exception{
+        FileInputStream fileInputStream = new FileInputStream(ROOT_PATH+"/output/1.txt");
+        FileChannel inputChannel = fileInputStream.getChannel();
+
+        FileOutputStream fileOutputStream = new FileOutputStream(ROOT_PATH+"/output/2.txt");
+        FileChannel outputChannel = fileOutputStream.getChannel();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(521);
+
+        while (true){
+            //重置标志位
+            byteBuffer.clear();
+            int read = inputChannel.read(byteBuffer);
+            if (read == -1) {
+                break;
+            }
+            //反转buffer
+            byteBuffer.flip();
+            outputChannel.write(byteBuffer);
+        }
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
